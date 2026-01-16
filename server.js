5ef69ac819dc67eead3af69bd46d2bd341e60045
Nephtys643 <boiscommunclement@gmail.com>
2026-01-16 16:07:30 +0100

    Refactor server.js to use ES6 import syntax

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // BASE SETUP
 // =============================================================================
+import express from 'express';
 
+const app = express();
 // call the packages we need
 var express    = require('express');
 var bodyParser = require('body-parser');
